69b8cb38c5cc328b0c016b1d19b8512f8f0e1974
Moritz Utcke <62291876+Letsmoe@users.noreply.github.com>
2023-04-23 18:46:51 +0400

    Update HeroCard.svelte

--- src/components/HeroCard.svelte
+++ src/components/HeroCard.svelte
@@ -6,24 +6,35 @@
 	export let reverse: boolean = false;
 </script>
 
-<section class="rounded-lg shadow-sm grid w-full h-auto border mb-20" class:reverse={reverse}>
+<section
+	class="rounded-lg shadow-sm grid w-full h-auto border mb-20"
+	class:reverse>
 	{#if reverse}
-	<img src={image} alt={alt} {width} class="ml-auto rounded-br-lg rounded-bl-lg lg:rounded-tr-lg lg:rounded-br-lg lg:rounded-bl-none h-full object-cover">
+		<img
+			src={image}
+			{alt}
+			{width}
+			class="ml-auto rounded-br-lg rounded-bl-lg lg:rounded-tr-lg lg:rounded-br-lg lg:rounded-bl-none h-full object-cover" />
 	{/if}
 	<div class="py-8 px-6">
-		<slot></slot>
+		<slot />
 	</div>
 	{#if !reverse}
-	<img src={image} alt={alt} {width} class="ml-auto rounded-br-lg rounded-bl-lg lg:rounded-tr-lg lg:rounded-br-lg lg:rounded-bl-none h-full object-cover">
+		<img
+			src={image}
+			{alt}
+			{width}
+			class="ml-auto rounded-br-lg rounded-bl-lg lg:rounded-tr-lg lg:rounded-br-lg lg:rounded-bl-none h-full object-cover" />
 	{/if}
 </section>
 
 <style lang="scss">
-	section:not(.reverse) {
-		@apply lg:grid-cols-[1fr,2fr];
-	}
-
-	.reverse {
-		@apply lg:grid-cols-[2fr,1fr];
+	@media (min-width: 1024px) {
+		section:not(.reverse) {
+			@apply grid-cols-[1fr,2fr];
+		}
+		.reverse {
+			@apply grid-cols-[2fr,1fr];
+		}
 	}
 </style>
