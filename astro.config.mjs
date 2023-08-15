cdaeec3fa5683319254215e9da31bebde5974a98
Moritz Utcke <62291876+Letsmoe@users.noreply.github.com>
2023-08-16 00:24:36 +0600

    Add Astro Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,25 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [svelte(), tailwind(), sitemap(), mdx()],
+  integrations: [svelte(), tailwind(), sitemap(), mdx(), compress({
+		Image: {
+			jpeg: {
+				quality: 80,
+			}
+		}
+	})],
   site: "https://cyclershub.com",
   output: "server",
   adapter: node({
